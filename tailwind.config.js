f4141ac9ec6827102a6e78eb5ae3b9639097131a
Dinesh Kumar <dineshkumarkrovvidi@gmail.com>
2026-03-24 15:43:03 +0530

    portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+],
   theme: {
     extend: {
       fontFamily: {
